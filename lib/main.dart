e0967a68209f50633795af8366f34c2998c5a3df
poya <poya1381@gmail.com>
2023-07-24 13:05:38 +0330

    refactor: setup system chrome and chagne splash ux

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,29 @@
 import 'package:apple_shop/config/theme/app_theme.dart';
+import 'package:apple_shop/core/constants/custom_colors.dart';
 import 'package:apple_shop/features/feat-product/data/models/product_model.dart';
 import 'package:apple_shop/features/feat_auth/presentation/screens/splash_screen.dart';
 import 'package:apple_shop/features/feat_basket/data/models/basket_model.dart';
 import 'package:apple_shop/features/feat_favorite/presentation/bloc/favorite_bloc.dart';
 import 'package:apple_shop/locator.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  // setup system chrome
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+  SystemChrome.setSystemUIOverlayStyle(
+    const SystemUiOverlayStyle(
+      statusBarColor: CustomColors.bgColor,
+      statusBarIconBrightness: Brightness.dark,
+      systemNavigationBarColor: CustomColors.bgColor,
+      systemNavigationBarIconBrightness: Brightness.dark,
+    ),
+  );
 
   // initalize hive
   await Hive.initFlutter();
